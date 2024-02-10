b3b7c99b7d7a5ff9d9ad75316c2982d99c7aec76
Mohamed-Eltobgy <mohamedhassan17602@yahoo.com>
2024-02-10 21:58:30 +0200

    Setting up GUI elements

---
+++ GuessingGame/src/main/java/com/example/guessinggame/GuessApplication.java
@@ -0,0 +1,27 @@
+package com.example.guessinggame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GuessApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GuessApplication.class.getResource("Guess-View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Image icon = new Image(getClass().getResource(("icon.png")).toExternalForm());
+        stage.getIcons().add(icon);
+        stage.setTitle("Guess The Number");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
